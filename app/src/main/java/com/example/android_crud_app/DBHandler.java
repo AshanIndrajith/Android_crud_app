f56090767150c1740dfd82f5e964e3fbc0a4982a
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-10-19 11:01:15 +0530

    databse connection

--- app/src/main/java/com/example/android_crud_app/DBHandler.java
+++ app/src/main/java/com/example/android_crud_app/DBHandler.java
@@ -1,12 +1,22 @@
 package com.example.android_crud_app;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHandler extends SQLiteOpenHelper {
+
+    private static final String databaseName="employee_details";
+    private static final int ver=1;
+
+    public DBHandler (Context context){
+        super(context,databaseName,null,ver);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String sql="CREATE TABLE employee(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, dep TEXT, salary TEXT);";
+        
     }
 
     @Override
