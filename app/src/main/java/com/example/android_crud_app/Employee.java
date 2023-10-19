0082b5020ae4cbe6ad5944b855f5ffcc8b1d99a7
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-10-19 11:10:33 +0530

    created Entity class as employee

---
+++ app/src/main/java/com/example/android_crud_app/Employee.java
@@ -0,0 +1,47 @@
+package com.example.android_crud_app;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String dep;
+    private String salary;
+
+    public Employee(int id, String name, String dep, String salary) {
+        this.id = id;
+        this.name = name;
+        this.dep = dep;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDep() {
+        return dep;
+    }
+
+    public void setDep(String dep) {
+        this.dep = dep;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
