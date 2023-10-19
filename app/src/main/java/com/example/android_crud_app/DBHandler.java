f463ca0903a184855c9ff59723f4b65668088b23
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-10-19 10:52:02 +0530

    update

---
+++ app/src/main/java/com/example/android_crud_app/DBHandler.java
@@ -0,0 +1,16 @@
+package com.example.android_crud_app;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
